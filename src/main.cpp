8d73c3d06266d49dcaf2c6ab06c7c278d243d303
Serdar Çoruhlu <serdar.coruhlu@hotmail.com>
2026-09-05 14:09:48 +0300

    hello window

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,20 @@
-#include <iostream>
+// #include <iostream>
+namespace SDL {
+    #include <SDL3/SDL.h>
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    SDL::SDL_Init(SDL_INIT_VIDEO);
+    SDL::SDL_Window* win = SDL::SDL_CreateWindow("russ - dev", 800, 600, 0);
+
+    SDL::SDL_Event event;
+    for (;;) {
+        while (SDL::SDL_PollEvent(&event)) {
+            if (event.type == SDL::SDL_EVENT_QUIT) goto loop_end;
+        }
+    }
+    loop_end:
+
+    SDL::SDL_DestroyWindow(win);
     return 0;
 }
