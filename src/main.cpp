a7fed7b952b8e2fda72573dcc7e838059cf729a0
Serdar Çoruhlu <serdar.coruhlu@hotmail.com>
2026-09-05 15:46:53 +0300

    implement generic line rasterization

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,66 @@
-#include <vector>
-// #include <iostream>
+#include <math.h>
+
 namespace SDL {
     #include <SDL3/SDL.h>
 }
 
-void lineRasterisation(int x0, int x1, int y0, int y1, SDL::SDL_Surface* image) {
+void drawLineHigh(SDL::SDL_Surface* surface, int x0, int x1, int y0, int y1, SDL::SDL_Color color) {
     int dx = x1 - x0;
     int dy = y1 - y0;
-    int D = 2 * dy - dx;
+    int xi = 1;
+    if (dx < 0) {
+        xi = -1;
+        dx = -dx;
+    }
+    int D = (2 * dx) - dy;
+    int x = x0;
+
+    for (int y = y0; y < y1; y++) {
+        SDL::SDL_WriteSurfacePixel(surface, x, y, color.r, color.g, color.b, color.a);
+        if (D > 0) {
+            x += xi;
+            D += (2 * (dx - dy));
+        } else {
+            D += 2 * dx;
+        }
+    }
+}
+
+void drawLineLow(SDL::SDL_Surface* surface, int x0, int x1, int y0, int y1, SDL::SDL_Color color) {
+
+    int dx = x1 - x0;
+    int dy = y1 - y0;
+    int yi = 1;
+    if (dy < 0) {
+        yi = -1;
+        dy = -dy;
+    }
+    int D = (2 * dy) - dx;
     int y = y0;
 
     for (int x = x0; x < x1; x++) {
-        SDL::SDL_WriteSurfacePixel(image, x, y, 0, 255, 0 , 255);
-        if (D >= 0) {
-            y++;
-            D += 2 * (dy - dx);
-        }
-        else {
+        SDL::SDL_WriteSurfacePixel(surface, x, y, color.r, color.g, color.b, color.a);
+        if (D > 0) {
+            y += yi;
+            D += (2 * (dy - dx));
+        } else {
             D += 2 * dy;
+        }
+    }
+}
+
+void drawLine(SDL::SDL_Surface* surface, int x0, int x1, int y0, int y1, SDL::SDL_Color color) {
+    if (abs(y1 - y0) < abs(x1 - x0)) {
+        if (x0 > x1) {
+            drawLineLow(surface, x1, x0, y1, y0, color);
+        } else {
+            drawLineLow(surface, x0, x1, y0, y1, color);
+        }
+    } else {
+        if (y0 > y1) {
+            drawLineHigh(surface, x1, x0, y1, y0, color);
+        } else {
+            drawLineHigh(surface, x0, x0, y0, y1, color);
         }
     }
 }
@@ -28,7 +71,12 @@ int main() {
 
     SDL::SDL_Event event;
     SDL::SDL_Surface* surface = SDL::SDL_GetWindowSurface(win);
-    lineRasterisation(15, 300, 20, 180, surface);
+
+    drawLine(surface, 15,  300, 50,  180, {0, 255, 0, 255});
+    drawLine(surface, 585, 300, 50,  180, {0, 255, 0, 255});
+    drawLine(surface, 15,  300, 310, 180, {0, 255, 0, 255});
+    drawLine(surface, 585, 300, 310, 180, {0, 255, 0, 255});
+    drawLine(surface, 585, 15, 310, 310,  {255, 0, 0, 255});
 
     for (;;) {
         while (SDL::SDL_PollEvent(&event)) {
